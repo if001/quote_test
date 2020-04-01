1d797868780354c61096d78d877a675f84928daa
if001 <otomijuf.004@gmail.com>
2020-04-01 18:50:05 +0900

    fix

--- server/es-ws.ts
+++ server/es-ws.ts
@@ -3,9 +3,8 @@ import expressWs from 'express-ws';
 
 const app = express();
 // const hostname = process.env.wsHOST || '127.0.0.1';
-// const port = process.env.wsPORT || 8000;
 const hostname = 'ws://mystifying-mclean-b57411.netlify.com';
-const port = 443;
+const port = process.env.wsPORT || 8000;
 
 // WebSocket用のエンドポイントを追加
 const ws = expressWs(app);
